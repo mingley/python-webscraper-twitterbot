3a475e6c030fd141405d1f21409a8cb309e7365a
mikasd <michael.ingley@gmail.com>
2019-10-30 10:07:33 -0700

    twitter bot portion done

--- twitter-bot.py
+++ twitter-bot.py
@@ -4,14 +4,20 @@ import tweepy as tp
 import time
 import os
 
-#api keys
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_secret = ''
+#twitter dev keys here
+consumer_key = 'CONSUMER_KEY'
+consumer_secret = 'CONSUMER_SECRET'
+access_token = 'ACCESS_TOKEN'
+access_secret = 'ACCESS_SECRET'
 
 #login to twitter account api
-auth = tp.OAuthHandler(consumer_key, )
+auth = tp.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+api = tp.API(auth)
 
+#for loop to iterate through pictures, posts one dog picture every 20 seconds to twitter
+os.chdir('dogs')
+for dog_image in os.listdir('.'):
+    api.upate_with_media(dog_image)
+    time.sleep(20)
 
-####TBD
